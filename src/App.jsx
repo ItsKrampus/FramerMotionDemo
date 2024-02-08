ecb7f81a4a50b652e1534fa33063a134eed809d4
ItsKrampus <106743990+ItsKrampus@users.noreply.github.com>
2024-02-08 19:29:57 +0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 import Box from './components/box'
 import Button from './components/Button'
 import AnimatedButton from './components/AnimatedButton'
+import InViewAnimations from './components/InViewAnimations'
 function App() {
 
 
   return (
     <>
-      <AnimatedButton />
+      <InViewAnimations/>
     </>
   )
 }
